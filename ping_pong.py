6b65746345dbf5418da960eda3d479e2a2c4155e
joseperdom0 <aguilar.roy@gmail.com>
2022-02-22 22:49:53 -0500

    Added pingpong game

---
+++ ping_pong.py
@@ -0,0 +1,13 @@
+from turtle import Screen, Turtle
+screen = Screen()
+
+class Paddle(Turtle):
+    
+
+
+
+
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONG!")
+screen.exitonclick()
