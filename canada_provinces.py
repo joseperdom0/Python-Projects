46ce2e42951bbc1998b264af00700cabf3b8e795
joseperdom0 <aguilar.roy@gmail.com>
2022-03-11 00:39:14 -0500

    initial commit canada_provinces.py

--- canada_provinces.py
+++ canada_provinces.py
@@ -1,1 +1,9 @@
-import
+import turtle
+screen = turtle.Screen()
+screen.title("Canada Provinces")
+image = "canada_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_province = screen.textinput(title="Guess the Province",prompt="Type the Province name:")
+ 
