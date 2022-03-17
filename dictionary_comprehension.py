5e3e4b80007edf605884806eb327f090c17e5966
joseperdom0 <aguilar.roy@gmail.com>
2022-03-16 23:54:56 -0400

    Added dictionary_comprehension.py

---
+++ dictionary_comprehension.py
@@ -0,0 +1,47 @@
+import random
+import pandas
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+
+student_scores = {name: random.randint(1, 100) for name in names}
+print(student_scores)
+
+passed_students = {key: value for (key, value) in student_scores.items() if value > 59}
+print(passed_students)
+
+# Exercise 01
+sentence = "What is the airspeed velocity of an Unladen Swallow"
+
+count_words = {word: len(word) for word in sentence.split()}
+print(count_words)
+
+# exercise 02
+# (temp_c * 9/5) + 32 = temp_f
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day: ((temp_c * 9/5) + 32) for (day, temp_c) in weather_c.items()}
+print(weather_f)
+
+
+# From pandas DataFrame
+
+student_dict = {
+    "student" : ["Angela", "James", "Lily"],
+    "score" : [56, 76, 98]
+}
+
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+for (index, row ) in student_data_frame.iterrows():
+    #print(row.score)
+    print(row)
--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,25 @@
-numbers = [1,2,3,4]
-new_list = [n+1 for n in numbers]
+import pandas
+
+numbers = [1, 2, 3, 4]
+new_list = [n + 1 for n in numbers]
 print(new_list)
+
+new_range = [n * 2 for n in range(1, 5)]
+print(new_range)
+
+ex_numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [n * n for n in ex_numbers]
+print(squared_numbers)
+
+even_numbers = [num for num in ex_numbers if num % 2 == 0]
+print(even_numbers)
+
+list_file1 = pandas.read_csv("file1.txt", sep= ' ', header= None)
+list_1 = list_file1[0].to_list()
+
+list_file2 = pandas.read_csv("file2.txt", sep= ' ', header= None)
+list_2 = list_file2[0].to_list()
+# print(list_1, list_2)
+
+occurrence = [num for num in list_1 if num in list_2]
+print(occurrence)
