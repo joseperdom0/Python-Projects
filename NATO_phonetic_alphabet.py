726fed363284a308a0b10ad29b3d164de5866c1a
joseperdom0 <aguilar.roy@gmail.com>
2022-03-17 11:33:04 -0400

    completed project nato_phonetic_alphabet.py

--- NATO_phonetic_alphabet.py
+++ NATO_phonetic_alphabet.py
@@ -1,5 +1,20 @@
 import pandas
 
 file = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(file)
+df = pandas.DataFrame(file)
+# print(file)
+# print(df)
 
+nato_alphabet = {row.letter: row.code for (index, row) in df.iterrows()}
+print(nato_alphabet)
+
+word = input("Please enter your word to decode: ").upper()
+decoded =  []
+# for char in word:
+#     for (key, value) in nato_alphabet.items():
+#         if char.title() == key:
+#             decoded.append(value)
+
+decoded = [nato_alphabet[char] for char in word]
+
+print(decoded)
--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -43,5 +43,5 @@ student_data_frame = pandas.DataFrame(student_dict)
 print(student_data_frame)
 
 for (index, row ) in student_data_frame.iterrows():
-    #print(row.score)
-    print(row)
+    print(row.score)
+    # print(row)
