6268d00df1f49b870679e239a26c5468353bc9df
joseperdom0 <aguilar.roy@gmail.com>
2022-03-24 23:26:10 -0400

    completed functionality unit_converter.py

--- unit_converter.py
+++ unit_converter.py
@@ -1,7 +1,30 @@
 import tkinter
 
+def calculate():
+    miles = float(user_input.get())
+    lbl_kilometers["text"] = miles * 1.609
+
+
 window = tkinter.Tk()
 window.title("Unit Converter")
 window.minsize(width=200, height=200)
+window.config(padx=20, pady=20)
+
+user_input = tkinter.Entry(width=20)
+user_input.pack()
+
+label1 = tkinter.Label(text="Miles")
+label1.pack()
+
+lbl_kilometers = tkinter.Label(text="")
+lbl_kilometers.pack()
+
+label2 = tkinter.Label(text="Kilometers")
+label2.pack()
+
+btn_calculate = tkinter.Button(text="Calculate", command=calculate)
+btn_calculate.pack()
+
 
 window.mainloop()
+
