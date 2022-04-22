a1b8109477b3dc12d49a975bddec677f8125bd78
joseperdom0 <aguilar.roy@gmail.com>
2022-04-21 22:42:09 -0400

    solver hyperskill daily problem

--- hyperskill_sandbox.py
+++ hyperskill_sandbox.py
@@ -1,4 +1,12 @@
-dividend = 10000000000
-divisor = 5
-
-print((int(dividend) / int(divisor)) % 2 != 0)
+#A = input()
+#B = input()
+#H = input()
+A = 6
+B = 10
+H = 8
+if H < A:
+    print("Deficiency")
+elif H > B:
+    print("Excess")
+else:
+    print("Normal")
