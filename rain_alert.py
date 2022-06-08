72961c9843763411ba7ed053651fbc8dacbc949c
joseperdom0 <aguilar.roy@gmail.com>
2022-06-07 23:51:01 -0400

    added own lat and long to API call

--- rain_alert.py
+++ rain_alert.py
@@ -1,1 +1,11 @@
+import requests
+
 API_KEY = "fc5fc9e9867b9707e3adc54517440b8d"
+LAT = "43.653225"
+LONG = "-79.383186"
+url_api = "https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"&appid="
+
+api_response = requests.get(url= url_api+API_KEY)
+
+print(api_response)
+print(api_response.json())
