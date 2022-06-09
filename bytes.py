ae75bc717d385d290c716cbce4c695f3ac9e42cf
joseperdom0 <aguilar.roy@gmail.com>
2022-06-09 19:27:10 -0400

    TODO twilio API

--- bytes.py
+++ bytes.py
@@ -1,6 +1,26 @@
-first_bytes = b'123'
-print(first_bytes)       # b'123'
-print(len(first_bytes))  # 3
-print(first_bytes[0])  # 49
-print(first_bytes[1])  # 50
-print(first_bytes[2])  # 51
+# first_bytes = b'123'
+# print(first_bytes)       # b'123'
+# print(len(first_bytes))  # 3
+# print(first_bytes[0])  # 49
+# print(first_bytes[1])  # 50
+# print(first_bytes[2])  # 51
+#
+# characters = bytes([55, 255])
+# print(characters)  # b'7\xff'
+
+
+# clear_text = input()
+# cypher = ""
+# for char in clear_text:
+#     #print(ord(char))
+#     new_ord = ord(char) + 1
+#     #print(char)
+#     cypher += chr(new_ord)
+#
+# print(cypher)
+
+# char_1 = input()
+# char_2 = input()
+# num_sum = ord(char_1) + ord(char_2)
+# print(num_sum)
+print(bytes([126]))
--- rain_alert.py
+++ rain_alert.py
@@ -9,9 +9,18 @@ weather_params = {
     "lat": LAT,
     "lon": LONG,
     "appid": API_KEY,
+    "exclude": "current,minutely,daily"
 }
 
 api_response = requests.get(ENDPOINT, params= weather_params)
 
 print(api_response.status_code)
 print(api_response.json())
+
+for hour in range(12):
+    print(api_response.json()['hourly'][hour]['weather'][0]['id'])
+    if api_response.json()['hourly'][hour]['weather'][0]['id'] < 700:
+        print('Bring an umbrella')
+
+#TODO import twilio.rest etc
+# https://www.twilio.com/
