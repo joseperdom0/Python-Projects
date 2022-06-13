f6d43c014b42e23e79964dad6aefe592a15f5c23
joseperdom0 <aguilar.roy@gmail.com>
2022-06-12 21:35:18 -0400

    modified sandbox

--- hyperskill_sandbox.py
+++ hyperskill_sandbox.py
@@ -1,15 +1,1 @@
-# Save the input in this variable
-ticket = "999999"
-digits = [int(num) for num in str(ticket)]
-
-
-# Add up the digits for each half
-half1 = sum(digits[0:3])
-half2 = sum(digits[-3::])
-
-print(half1,half2)
-# Thanks to you, this code will work
-if half1 == half2:
-    print("Lucky")
-else:
-    print("Ordinary")
+print(round(2.675, 2))
