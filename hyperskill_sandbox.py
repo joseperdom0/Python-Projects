d90e4fa73ffb887a58d3965fc6b3df4e4bf1f6c9
joseperdom0 <aguilar.roy@gmail.com>
2022-06-30 08:25:46 -0400

    modified hyperskill sandbox

--- hyperskill_sandbox.py
+++ hyperskill_sandbox.py
@@ -1,20 +1,10 @@
-# Example 1
-tuple_1 = (1, 2, 3)
-for elem in tuple_1:
-    print(elem)
+this_list = ['Mary', 'had', 'a', 'little', 'lamb']
 
-# Example 2
-tuple_2 = (1, 2, 3, 4)
-print(5 in tuple_2)
-print(5 not in tuple_2)
 
-# Example 3
-tuple_3 = (1, 2, 3, 5)
-print(len(tuple_3))
+def my_list(this_list):
+    del this_list[3]
+    this_list[3] = 'ram'
 
-# Example 4
-tuple_4 = tuple_1 + tuple_2
-tuple_5 = tuple_3 * 2
 
-print(tuple_4)
-print(tuple_5)
+print(my_list(this_list))
+print(this_list)
