1321e9b016425f516cde06c37f90d26bda4a520d
joseperdom0 <aguilar.roy@gmail.com>
2022-07-01 13:55:21 -0400

    added tuple question PE exam

--- tuple_as_return_PE_Q23.py
+++ tuple_as_return_PE_Q23.py
@@ -2,7 +2,8 @@ def runner(brand, model ="", year =2021, convertible = False):
     var = (brand, str(year),str(convertible))
     print(var)
     print(type(var))
-    return(brand, str(year),str(convertible))
+    return brand, str(year), str(convertible)
+
 
 print(runner("Fermi")[2][2])
 
